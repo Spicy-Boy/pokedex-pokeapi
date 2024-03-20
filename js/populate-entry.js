52b815da56ad3949248dad7786a4f035c3807ffb
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-03-20 11:22:27 -0400

    Set up grid, entry header, classification

--- js/populate-entry.js
+++ js/populate-entry.js
@@ -1,15 +1,21 @@
-const url = "https://pokeapi.co/api/v2/pokemon/";
+let url = "https://pokeapi.co/api/v2/pokemon/"; //game data
 
 const searchParams = new URLSearchParams(window.location.search);
+let pokemonID = searchParams.get("pokemon");
 
 //INITIALIZE DOM OBJECTS!
+const iconSprite = document.getElementById('icon-sprite');
+const pokeIndex = document.getElementById('poke-index');
+
 const sprite = document.getElementById('sprite');
-const pokeName = document.getElementById('name');
+const name1 = document.getElementById('name1');
+const name2 = document.getElementById('name2');
+const classification = document.getElementById('classification');
 const type = document.getElementById('type');
 
 //get the pokemon's pokedex index from url parameter called "pokemon" vvv
 console.log("Search parameter value: ", searchParams.get("pokemon"));
-let pokemonID = searchParams.get("pokemon")
+
 fetch(url+pokemonID)
 .then(json => json.json() )
 .then( obj => {
@@ -17,10 +23,16 @@ fetch(url+pokemonID)
     console.log(obj.name);
     console.log(obj);
 
+    //inside entry-header 
+    iconSprite.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/"+pokemonID+".png";
+    pokeIndex.textContent = "#"+pokemonID+"  ";
+
+    //inside entry-animated
     //fetched img, name, and type
-    let spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+pokemonID+".gif" //animated implied
+    let spriteUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+pokemonID+".gif"; //animated implied
     sprite.src = spriteUrl;
-    pokeName.textContent = capitalizeFirstLetter(obj.name);
+    name1.textContent = capitalizeFirstLetter(obj.name);
+    name2.textContent = capitalizeFirstLetter(obj.name);
 
     //types arrive as an array of objects
     let types = obj.types;
@@ -35,7 +47,14 @@ fetch(url+pokemonID)
         {
             type.textContent += ", "+capitalizeFirstLetter(e.type.name);
         }
-
     });
+});
 
+url = "https://pokeapi.co/api/v2/pokemon-species/"
+
+fetch(url+pokemonID)
+.then(json => json.json() )
+.then( obj => {
+    console.log("Genera:",obj.genera[7].genus);
+    classification.textContent = obj.genera[7].genus;
 });
