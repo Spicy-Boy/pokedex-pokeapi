9ad5e2979646b2c812c6d96f847023065208d4da
Spicy-Boy <46385369+Spicy-Boy@users.noreply.github.com>
2024-03-20 22:28:05 -0400

    WIP: format entryLore contents, flavor text

--- js/populate-entry.js
+++ js/populate-entry.js
@@ -13,6 +13,8 @@ const name2 = document.getElementById('name2');
 const classification = document.getElementById('classification');
 const type = document.getElementById('type');
 
+const entryLore = document.getElementById("entry-lore");
+
 //get the pokemon's pokedex index from url parameter called "pokemon" vvv
 console.log("Search parameter value: ", searchParams.get("pokemon"));
 
@@ -54,6 +56,44 @@ url = "https://pokeapi.co/api/v2/pokemon-species/"
 fetch(url+pokemonID)
 .then(json => json.json() )
 .then( obj => {
-    console.log("Genera:",obj.genera[7].genus);
+    console.log("Pokemon species data:",obj);
+    // console.log("Genera:",obj.genera[7].genus);
     classification.textContent = obj.genera[7].genus;
+
+    // console.log("flavor:", obj.flavor_text_entries);
+    for (let i = 0; i < obj.flavor_text_entries.length; i++)
+    {
+        let entry = obj.flavor_text_entries[i];
+        let previousEntryFlavor = "";
+        // console.log(entry);
+        // console.log(entry.version.name);
+        if (entry.version.name == "ruby" ||
+        entry.version.name == "sapphire" ||
+        entry.version.name == "emerald" ||
+        entry.version.name == "firered" ||
+        entry.version.name == "leafgreen" ||
+        entry.version.name == "diamond" ||
+        entry.version.name == "pearl" ||
+        entry.version.name == "platinum"
+        ){
+
+            let entryDiv = document.createElement('div');
+            entryDiv.classList.add('lore-individual');
+
+            let versionDiv = document.createElement('div');
+            versionDiv.classList.add("lore-version");
+            versionDiv.innerText = entry.version.name;
+            entryDiv.appendChild(versionDiv);
+
+            let flavorDiv = document.createElement('div');
+            flavorDiv.innerText = entry.flavor_text;
+            entryDiv.appendChild(flavorDiv);
+
+            if (entry.flavor_text != previousEntryFlavor)
+            {
+                previousEntry = entry.flavor_text;
+                entryLore.appendChild(entryDiv)
+            }   
+        }
+    }
 });
